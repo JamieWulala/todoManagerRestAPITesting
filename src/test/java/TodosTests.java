03898e527dcd4824e7a26c85e73240ef5cf2ca6b
JamieWulala <jamie.laevatein@gmail.com>
2020-10-21 14:51:54 -0400

    add jar application set up

--- src/test/java/TodosTests.java
+++ src/test/java/TodosTests.java
@@ -1,15 +1,39 @@
 import io.restassured.RestAssured;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.InputStream;
 
 import static io.restassured.RestAssured.when;
 
 public class TodosTests {
 
+    private static Process proc;
+
     @BeforeClass
     public static void setup() {
         //Set base URI
         RestAssured.baseURI = "http://localhost:4567";
+        System.out.println("------Set Up------");
+        //start the application
+        try {
+            System.out.println("Starting application......");
+            proc = Runtime.getRuntime().exec("java -jar runTodoManagerRestAPI-1.5.5.jar");
+            InputStream in = proc.getInputStream();
+            InputStream err = proc.getErrorStream();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void cleanUp(){
+        //stop the application
+        System.out.println("------Clean Up------");
+        System.out.println("Application Stopped");
+        proc.destroy();
     }
 
     @Test
@@ -18,6 +42,21 @@ public class TodosTests {
 
         when().
                 get("/todos").
+                then()
+                .statusCode(200);
+
+        when().
+                get("/todos/{id}", 1).
+                then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void
+    testPoseToDos() {
+
+        when().
+                post("/todos").
                 then()
                 .statusCode(200);
     }
