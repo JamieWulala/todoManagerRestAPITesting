b2e7db424106fc03161552ea757d887e51347ecb
Chris Li <chris1e686@gmail.com>
2020-10-21 22:18:27 -0400

    update template code

--- src/test/java/TodosTests.java
+++ src/test/java/TodosTests.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.lang.*;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
 
 public class TodosTests {
 
@@ -32,7 +34,7 @@ public class TodosTests {
     }
 
     @AfterClass
-    public static void cleanUp(){
+    public static void cleanUp() {
         //stop the application
         System.out.println("------Clean Up------");
         System.out.println("Application Stopped");
@@ -40,33 +42,35 @@ public class TodosTests {
     }
 
     @Test
-    public void
-    testGetToDos() {
-
+    public void testGetTodos() {
+        //get todos list
         when().
                 get("/todos").
-                then()
-                .statusCode(200);
-
+        then().
+                statusCode(200);
+        //get a todo item
         when().
                 get("/todos/{id}", 1).
-                then()
-                .statusCode(200);
+        then().
+                statusCode(200).
+                body("todos.get(0).title", equalTo("scan paperwork")).
+                body("todos.get(0).id", equalTo("1"));
     }
 
     @Test
-    public void
-    testPoseToDos() {
+    public void testPoseTodos() {
+        //request body
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("title", "someTitle");
 
-        Map<String, String> request = new HashMap<>();
-        request.put("title", "someTitle");
-
-        given()
-                .contentType("application/json")
-                .body(request).
-        when()
-                .post("/todos")
-                .then()
-                .statusCode(201);
+        //post a todo item
+        given().
+                contentType("application/json").
+                body(requestBody).
+        when().
+                post("/todos").
+        then().
+                statusCode(201).
+                body("title", equalTo("someTitle"));
     }
 }
